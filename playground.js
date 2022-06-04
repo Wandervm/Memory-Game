6397205efc22fe64c5649e8f84ef78442744fce9
Wander <wander.v.dvp@gmail.com>
2022-06-04 00:43:57 -0300

    Upload

---
+++ playground.js
@@ -0,0 +1,11 @@
+const setModal = document.getElementById("modalGame");
+const closeModal = setModal;
+
+document.getElementById("btn").onclick = function(){
+    setModal.style.display = "block";
+    
+}
+document.getElementById("close").onclick = function(){
+    setModal.style.display = "none"
+}
+
