8833e3990ad566ebc43ab40a8e6f5d14bf6f9e37
Wander <wander.v.dvp@gmail.com>
2022-06-04 02:31:09 -0300

    Att

--- playground.js
+++ playground.js
@@ -1,5 +1,44 @@
 const setModal = document.getElementById("modalGame");
 const closeModal = setModal;
+const cards = document.querySelectorAll('.cards');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+function flipCard() {
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+
+    checkMatch();
+}
+
+cards.forEach((card) =>{
+    card.addEventListener('click', flipCard)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementById("btn").onclick = function(){
     setModal.style.display = "block";
@@ -8,4 +47,7 @@ document.getElementById("btn").onclick = function(){
 document.getElementById("close").onclick = function(){
     setModal.style.display = "none"
 }
+
+
+
 
