65780202c9c8646e964538bb173e39255033826e
Wander <wander.v.dvp@gmail.com>
2022-06-18 14:05:55 -0300

    Playground

--- playground.js
+++ playground.js
@@ -11,5 +11,42 @@ document.getElementById("close").onclick = function(){
 }
 
 
+//DarkMode and LigthMode
+
+const buttonLight = document.getElementById('mode-select');
+const body = document.getElementById('body');
+const playScreen = document.getElementById('screenPlay');
 
 
+
+buttonLight.addEventListener('click', modeChange);
+
+
+function modeChange(){
+    changeClass();
+    darkButton();
+    changetext();
+   
+}
+
+function changeClass(){
+    
+    body.classList.toggle( 'lightMode');
+   
+}
+function darkButton(){
+  
+    buttonLight.classList.toggle( 'darkButton');
+}
+
+function changetext(){
+    if(body.classList.contains('lightMode')){
+        buttonLight.innerHTML = 'Dark Mode';
+        return;
+    }
+    if (body.classList.contains('darkMode')){
+        buttonLight.innerHTML = "Light Mode";
+        return;
+        
+    }
+}
